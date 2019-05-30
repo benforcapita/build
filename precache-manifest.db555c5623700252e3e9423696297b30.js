d3ac0acc83d00eb270d5d5fe18391b3dd18367bf
Ben Blum <benforcapita@gmail.com>
2019-05-30 20:01:15 +0300

    first
    first

---
+++ precache-manifest.db555c5623700252e3e9423696297b30.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20ece3a8d49e71f32c1865ebbd358f1",
+    "url": "/webAPP/index.html"
+  },
+  {
+    "revision": "77bfb879946c13dee23b",
+    "url": "/webAPP/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1e343cbfb1c3f919d6c0",
+    "url": "/webAPP/static/css/main.e4107874.chunk.css"
+  },
+  {
+    "revision": "77bfb879946c13dee23b",
+    "url": "/webAPP/static/js/2.893a0230.chunk.js"
+  },
+  {
+    "revision": "1e343cbfb1c3f919d6c0",
+    "url": "/webAPP/static/js/main.133520b9.chunk.js"
+  },
+  {
+    "revision": "b724969ee24ff1eca3c9",
+    "url": "/webAPP/static/js/runtime~main.59442af7.js"
+  },
+  {
+    "revision": "c017cca178653786efb68dfefd529006",
+    "url": "/webAPP/static/media/first.c017cca1.jpeg"
+  },
+  {
+    "revision": "e1b63334fa208b81d8734a6974b2973a",
+    "url": "/webAPP/static/media/second.e1b63334.jpeg"
+  },
+  {
+    "revision": "7186f418c33010fee4fd779a6b4f1281",
+    "url": "/webAPP/static/media/third.7186f418.jpeg"
+  }
+]);
